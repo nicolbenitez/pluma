07abac3a989488a7ecb164a2316b4b21c726505c
nicolbenitez <beniteznicol041@gmail.com>
2025-10-30 11:18:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-// Crear estrellitas animadas en el fondo
-const numStars = 60;
+// Generar estrellitas animadas
+const numStars = 70;
 for (let i = 0; i < numStars; i++) {
   const star = document.createElement('div');
-  star.classList.add('star');
+  star.classList.add('estrella');
   star.style.top = Math.random() * 100 + '%';
   star.style.left = Math.random() * 100 + '%';
   star.style.animationDelay = Math.random() * 2 + 's';
